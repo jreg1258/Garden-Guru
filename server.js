d732d90b61000360a97ffa46337e59c1d37c9b02
Joey Register <josephregister1258@gmail.com>
2020-05-03 17:23:17 -0400

    updated server to handle build files

--- server.js
+++ server.js
@@ -22,10 +22,10 @@ const bodyParser = require('body-parser'),
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.use(express.static(path.join(__dirname, '../build')))
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../build'))
-})
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(passportControl.initialize())
